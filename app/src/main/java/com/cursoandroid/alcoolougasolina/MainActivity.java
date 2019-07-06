e3497039288a4600a4c9bfc3d4b7569cb7ecc0af
Renan Costa Alencar <renan.costaalencar@gmail.com>
2019-07-06 09:13:58 -0300

    #App4 - Álcool ou Gasolina - Final

--- app/src/main/java/com/cursoandroid/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/cursoandroid/alcoolougasolina/MainActivity.java
@@ -3,12 +3,50 @@ package com.cursoandroid.alcoolougasolina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText precoAlcool;
+    private EditText precoGasolina;
+    private Button botaoVerificar;
+    private TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        precoAlcool = findViewById(R.id.precoAlcoolId);
+        precoGasolina = findViewById(R.id.precoGasolinaId);
+        botaoVerificar = findViewById(R.id.botaoVerificarId);
+        textoResultado = findViewById(R.id.textoResultadoId);
+
+        botaoVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Recupera valores digitados
+                String textoPrecoAlcool = precoAlcool.getText().toString();
+                String textoPrecoGasolina = precoGasolina.getText().toString();
+
+                // Converter valores strings para números
+                Double valorAlcool = Double.parseDouble(textoPrecoAlcool);
+                Double valorGasolina = Double.parseDouble(textoPrecoGasolina);
+
+                // Alcool / Gasolina
+                Double resultado = valorAlcool / valorGasolina;
+
+                if (resultado >= 0.7) {
+                    // Gasolina
+                    textoResultado.setText("É melhor utilizar Gasolina.");
+                } else {
+                    // Alcool
+                    textoResultado.setText("É melhor utilizar Álcool.");
+                }
+            }
+        });
     }
 }
